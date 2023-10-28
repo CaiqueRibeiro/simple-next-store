9eaf03671b9d231fbdac046f1191c4b5d1119e34
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2023-10-28 18:31:49 -0300

    feature: main page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className={inter.className} lang="pt">
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
