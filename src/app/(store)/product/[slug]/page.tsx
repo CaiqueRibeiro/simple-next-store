dce84646c171c9c65f85bb0d525b57a8f8c025ac
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2023-10-29 00:00:09 -0300

    feature: opengraph for products and cart context

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
-import { api } from "@/data/api";
-import { Product } from "@/data/types/product";
+import Image from 'next/image'
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 
 interface ProductProps {
   params: {
@@ -12,51 +13,102 @@ async function getProduct(slug: string): Promise<Product> {
   const response = await api(`/products/${slug}`, {
     next: {
       revalidate: 60 * 60, // 1h
-    }
+    },
   })
   const products = await response.json()
   return products
 }
 
+// Memoization - As chamada HTTP duplicada serão evitadas, feita uma única vez
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug)
+  return {
+    title: product.title,
+  }
+}
+
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+  return products.map((product) => ({ slug: product.slug }))
+}
 
 export default async function ProductPage({ params }: ProductProps) {
   const product = await getProduct(params.slug)
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
-        <Image src={product.image} alt="" width={1000} height={1000} quality={100} />
+        <Image
+          src={product.image}
+          alt=""
+          width={1000}
+          height={1000}
+          quality={100}
+        />
       </div>
 
       <div className="flex flex-col justify-center px-12">
         <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
-        <p className="mt-2 leading-relaxed text-zinc-400">{product.description}</p>
+        <p className="mt-2 leading-relaxed text-zinc-400">
+          {product.description}
+        </p>
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py2.5 font-semibold shrink-0">
             {product.price.toLocaleString('pt-BR', {
               style: 'currency',
-              currency: 'BRL'
+              currency: 'BRL',
             })}
           </span>
-          <span className="text-sm text-zinc-400">em 12x s/juros de {(product.price / 12).toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-          })}</span>
+          <span className="text-sm text-zinc-400">
+            em 12x s/juros de{' '}
+            {(product.price / 12).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </span>
         </div>
 
         <div className="mt-8 space-y-4">
           <span className="block font-semibold">Tamanhos</span>
 
           <div className="flex gap-2">
-            <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-small font-semibold">P</button>
-            <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-small font-semibold">M</button>
-            <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-small font-semibold">G</button>
-            <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-small font-semibold">GG</button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-small font-semibold"
+            >
+              P
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-small font-semibold"
+            >
+              M
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-small font-semibold"
+            >
+              G
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-small font-semibold"
+            >
+              GG
+            </button>
           </div>
         </div>
 
-        <button type="button" className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white hover:bg-emerald-800">Adicionar ao carrinho</button>
+        <button
+          type="button"
+          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white hover:bg-emerald-800"
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     </div>
   )
--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -8,10 +8,10 @@ interface RequestParams {
 }
 
 export async function GET(_: Request, { params }: RequestParams) {
-  await new Promise(resolve => setTimeout(resolve, 1000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   const slug = z.string().parse(params.slug)
-  const product = data.products.find(product => product.slug === slug)
-  if(!product) {
+  const product = data.products.find((product) => product.slug === slug)
+  if (!product) {
     return Response.json({ message: 'Product not found' }, { status: 404 })
   }
   return Response.json(product)
